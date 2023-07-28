32ef13d72182fd82016592b5c5147508f9830a86
Everton Feitosa <feitosasantoseverton@gmail.com>
2023-07-28 16:50:06 -0300

    criação tela register

--- projetoIonic/src/app/register/register.page.ts
+++ projetoIonic/src/app/register/register.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,9 +13,14 @@ import { IonicModule } from '@ionic/angular';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  constructor(private route:Router) { }
 
   ngOnInit() {
   }
 
+  public home() {
+    setTimeout(() => {
+      this.route.navigate(['/home'])
+    }, 1000)
+  }
 }
